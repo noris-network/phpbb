484b7ad652af8e460cb803629c61817a921c5ee7
disappear89 <thomas-schwaig@web.de>
2017-04-27 17:11:15 +0200

    openshift build ready
    get configuration to db form env variables

---
+++ phpBB/config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.2.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\mysqli';
+$dbhost = $_ENV['DBHOST'];
+$dbport = $_ENV['DBPORT'];
+$dbname = $_ENV['DBNAME'];
+$dbuser = $_ENV['DBUSER'];
+$dbpasswd = $_ENV['DBPASSWD'];
+$table_prefix = $_ENV['TABLE_PREFIX'];
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
